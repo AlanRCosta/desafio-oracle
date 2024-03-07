231bea1ba3a60fcb2b5f2bd59cf147b313599610
Alan Costa <alanrodrigues.costa@outlook.com>
2024-03-07 05:24:06 -0300

    feat: Finishing the functions

--- scripts/home.js
+++ scripts/home.js
@@ -1,48 +1,94 @@
-const texto = "slcr";
+const isValidString = (string) => /^[a-z\s]+$/.test(string);
 
-function teste(texto) {
-  const conditions = { 0: "a", 1: "e", 2: "i", 3: "o", 4: "u" };
+const getInputValue = () => document.getElementById("inputText").value;
 
-  let frase = "";
+const stateChange = (text) => {
+  const isValid = isValidString(text);
+
+  if (isValid) {
+    document.getElementById("tipText").style.color = "#495057";
+    document.getElementById("tipText").style.fontSize = "0.75rem";
+    document.getElementById("tipText").style.fontWeight = "400";
+  } else {
+    document.getElementById("tipText").style.color = "#C51921";
+    document.getElementById("tipText").style.fontWeight = "bold";
+    document.getElementById("tipText").style.fontSize = "0.9rem";
+    document.getElementById("inputText").value = "";
+  }
+
+  if (text.length && isValid) {
+    document.getElementById("noMessage").style.display = "none";
+    document.getElementById("withMessage").style.display = "flex";
+  } else {
+    document.getElementById("noMessage").style.display = "flex";
+    document.getElementById("withMessage").style.display = "none";
+  }
+};
+
+const encryptAndDecrypt = (text, conditions, substitutions) => {
+  let modifiedText = "";
 
   if (
-    texto.includes(conditions[0]) ||
-    texto.includes(conditions[1]) ||
-    texto.includes(conditions[2]) ||
-    texto.includes(conditions[3]) ||
-    texto.includes(conditions[4])
+    text.includes(conditions[0]) ||
+    text.includes(conditions[1]) ||
+    text.includes(conditions[2]) ||
+    text.includes(conditions[3]) ||
+    text.includes(conditions[4])
   ) {
-    frase = texto
-      .replaceAll(conditions[0], "x")
-      .replaceAll(conditions[1], "x")
-      .replaceAll(conditions[2], "x")
-      .replaceAll(conditions[3], "x")
-      .replaceAll(conditions[4], "x");
+    modifiedText = text
+      .replaceAll(conditions[0], substitutions[0])
+      .replaceAll(conditions[1], substitutions[1])
+      .replaceAll(conditions[2], substitutions[2])
+      .replaceAll(conditions[3], substitutions[3])
+      .replaceAll(conditions[4], substitutions[4]);
   } else {
-    frase = texto;
+    modifiedText = text;
   }
-  return frase;
-}
+  return modifiedText;
+};
 
-console.log(teste(texto));
+const encryptText = () => {
+  const inputValue = getInputValue();
 
-// const getValue = () => {
-//   let inputValue = document.getElementById("inputText").value;
+  const conditions = { 0: "e", 1: "i", 2: "a", 3: "o", 4: "u" };
+  const substitutions = {
+    0: "enter",
+    1: "imes",
+    2: "ai",
+    3: "ober",
+    4: "ufat",
+  };
 
-//   stateChange(inputValue);
-// };
+  const encryptedText = encryptAndDecrypt(
+    inputValue,
+    conditions,
+    substitutions
+  );
 
-// const copyText = () => {
-//   document.querySelector("#outputText").select();
-//   document.execCommand("copy");
-// };
+  stateChange(inputValue);
 
-// const stateChange = (text) => {
-//   if (text.length) {
-//     document.getElementById("noMessage").style.display = "none";
-//     document.getElementById("withMessage").style.display = "flex";
-//   } else {
-//     document.getElementById("noMessage").style.display = "flex";
-//     document.getElementById("withMessage").style.display = "none";
-//   }
-// };
+  document.getElementById("outputText").value = encryptedText;
+};
+
+const decryptText = () => {
+  const inputValue = getInputValue();
+
+  const conditions = { 0: "enter", 1: "imes", 2: "ai", 3: "ober", 4: "ufat" };
+  const substitutions = { 0: "e", 1: "i", 2: "a", 3: "o", 4: "u" };
+
+  const decryptedText = encryptAndDecrypt(
+    inputValue,
+    conditions,
+    substitutions
+  );
+
+  stateChange(inputValue);
+
+  document.getElementById("outputText").value = decryptedText;
+};
+
+const copyText = () => {
+  document.querySelector("#outputText").select();
+  document.execCommand("copy");
+  document.getElementById("inputText").value = "";
+};
